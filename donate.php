ff7666c188509945ca9a9efced5027ab520782f4
Ian Maleney <imaleney@gmail.com>
2016-08-09 16:38:13 +0100

    Email Link
    Added link for support emails.

--- donate.php
+++ donate.php
@@ -11,7 +11,7 @@
         <p>Fallow Media is free. You can read everything online without getting a subscription. Still, we think it's very important to pay our contributors. We rely on readers to make sure that happens.</p>
         <p>There are two ways you can help us continue our work: You can donate, or subscribe.</p>
         <p>Donation is a simple, one-off way to send us some money &mdash; no amount is too little. Subscription is a monthly payment of just €3. Any money we recieve is used to pay contributors and to cover our minimal hosting costs.</p>
-        <p>We use Paypal to handle donations and subscription payments. If you have any trouble, email us and we'll sort you out.</p>
+        <p>We use Paypal to handle donations and subscription payments. If you have any trouble, <a href="mailto:info@fallowmedia.com">email us</a> and we'll sort you out.</p>
         <div id="subform">
           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
             <input type="hidden" name="cmd" value="_s-xclick">
